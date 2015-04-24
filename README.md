577afb72b4f74588f0ab25cf7eb2330ab1e8f70a
Amir Djavaherian <adjavaherian@hotpads.com>
2015-04-24 14:36:09 -0700

    exporting module for PageLoader

--- README.md
+++ README.md
@@ -7,3 +7,21 @@ cd webpack-react-router/
 npm install
 rm -rf dist/* && webpack --display-error-details && node index.js
 ```
+
+The following 'smoke tests' show that chunking is successful and vendor bundle contains the React module
+
+```
+adjavaherian@amir:~/Desktop/webpack-react-router$ grep -R Hero dist
+dist/1.chunk.js:	var Hero = __webpack_require__(3);
+dist/1.chunk.js:	            React.createElement(Hero, null)
+dist/2.chunk.js:	var Hero = __webpack_require__(3);
+dist/2.chunk.js:	            React.createElement(Hero, null),
+dist/vendor.bundle.js:	var Hero = React.createClass({
+dist/vendor.bundle.js:	    displayName: 'Hero',
+dist/vendor.bundle.js:	            { className: 'Hero' },
+dist/vendor.bundle.js:	module.exports = Hero;
+adjavaherian@amir:~/Desktop/webpack-react-router$ grep -R zzz dist
+dist/2.chunk.js:	            'zzzzzz'
+adjavaherian@amir:~/Desktop/webpack-react-router$ grep -R React.version dist
+dist/vendor.bundle.js:	React.version = '0.13.2';
+```
--- app/AppRoutes.jsx
+++ app/AppRoutes.jsx
@@ -2,50 +2,12 @@ var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
 var AppTemplate = require('./AppTemplate');
-
-var MobileAppsPage, FrontPage = null;
-
-var FrontPageLoader = React.createClass({
-
-    componentDidMount: function() {
-        var that = this;
-        require.ensure(['./pages/FrontPage'], function(require){
-            FrontPage = require('./pages/FrontPage');
-            that.forceUpdate();
-        });
-    },
-    render: function() {
-        if (FrontPage) {
-            return <FrontPage {...this.props} />;
-        } else {
-            return <div className="spinner" />;
-        }
-    }
-});
-
-
-var MobileAppsPageLoader = React.createClass({
-
-    componentDidMount: function() {
-        var that = this;
-        require.ensure(['./pages/MobileAppsPage'], function(require){
-            MobileAppsPage = require('./pages/MobileAppsPage');
-            that.forceUpdate();
-        });
-    },
-    render: function() {
-        if (MobileAppsPage) {
-            return <MobileAppsPage {...this.props} />;
-        } else {
-            return <div className="spinner" />;
-        }
-    }
-});
+var PageLoader = require('./PageLoader');
 
 var AppRoutes = (
     <Route path='/' handler={AppTemplate}>
-        <Route name="FrontPage" path='/' handler={FrontPageLoader}/>
-        <Route name="MobileAppsPage" path="mobile-apps" handler={MobileAppsPageLoader}/>
+        <Route name="FrontPage" path='/' handler={PageLoader.FrontPage}/>
+        <Route name="MobileAppsPage" path="mobile-apps" handler={PageLoader.MobileAppsPage}/>
     </Route>
 );
 
