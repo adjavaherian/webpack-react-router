deb7c064494351c34418b048cae40bb136eb79c3
Amir Djavaherian <adjavaherian@hotpads.com>
2015-04-24 14:37:24 -0700

    exporting module for PageLoader

---
+++ app/PageLoader.jsx
@@ -0,0 +1,47 @@
+var React = require('react');
+
+var FrontPage, MobileAppsPage = null;
+
+var FrontPageLoader = React.createClass({
+
+    componentDidMount: function() {
+        var that = this;
+        require.ensure(['./pages/FrontPage'], function(require){
+            FrontPage = require('./pages/FrontPage');
+            that.forceUpdate();
+        });
+    },
+    render: function() {
+        if (FrontPage) {
+            return <FrontPage {...this.props} />;
+        } else {
+            return <div className="spinner" />;
+        }
+    }
+});
+
+
+var MobileAppsPageLoader = React.createClass({
+
+    componentDidMount: function() {
+        var that = this;
+        require.ensure(['./pages/MobileAppsPage'], function(require){
+            MobileAppsPage = require('./pages/MobileAppsPage');
+            that.forceUpdate();
+        });
+    },
+    render: function() {
+        if (MobileAppsPage) {
+            return <MobileAppsPage {...this.props} />;
+        } else {
+            return <div className="spinner" />;
+        }
+    }
+});
+
+
+module.exports  = {
+    FrontPage: FrontPageLoader,
+    MobileAppsPage: MobileAppsPageLoader
+
+};
